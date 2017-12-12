2ad5ee43b3d6c8cd5fe2b20ee9d15c42d445c42e
tongifer <tpow@sas.upenn.edu>
2017-12-12 14:44:48 -0500

    Ideas for p2_train

--- p2/p2_train.py
+++ p2/p2_train.py
@@ -5,6 +5,7 @@
 '''
 
 import PyNet as net
+import numpy as np
 from p2_utils import *
 
 
@@ -14,22 +15,24 @@ from p2_utils import *
 '''
 
 layer_list = [
-                net.Conv2d(.........),
-                net.BatchNorm2D(.....),
-                net.Relu()
-                ......
-             ]
+    #net.Conv2d(16, 7, padding=0, stride=1),
+    # net.BatchNorm2D(),
+    # net.Relu(),
+    #net.Conv2d(8, 7, padding=0, stride=1),
+    # net.BatchNorm2D(),
+    # net.Relu(),
+    # net.Flatten(),
+    # net.Linear(100 * 128, 10)
+]
 
 '''
   Define loss function
 '''
-loss_layer = net.Loss_Function_name(......)
-
+loss_layer = net.L2_loss()
 '''
   Define optimizer 
 '''
-optimizer = net.optimizer_name(.......)
-
+optimizer = net.SGD_Optimizer(lr_rate=0.1, weight_decay=5e-4, momentum=0.99)
 
 '''
   Build model
@@ -39,9 +42,7 @@ my_model = net.Model(layer_list, loss_layer, optimizer)
 '''
   Define the number of input channel and initialize the model
 '''
-my_model.set_input_channel(...)
-
-
+my_model.set_input_channel(3)
 
 '''
   Main training process
@@ -49,18 +50,19 @@ my_model.set_input_channel(...)
     that is, total number of data = M * batch_size, each epoch need to traverse all data.
 '''
 
-# obtain data 
+# obtain data
 [data_set, label_set] = loadData(.....)
 
-for i in range (max_epoch_num):
+for i in range(1000):
   '''
     random shuffle data 
   '''
-  data_set_cur, label_set_cur = randomShuffle(data_set, label_set) # design function by yourself
-
+  data_set_cur, label_set_cur = randomShuffle(data_set, label_set)  # design function by yourself
 
   step = ...  # step is a int number
-  for j in range (step):
+  # step is the iteration needed to run through all the data, step = data_num/batch_size.
+  # You can drop the last batch if data_num is not divisible by the batch_size
+  for j in range(step):
     # obtain a mini batch for this step training
     [data_bt, label_bt] = obtainMiniBatch(....)  # design function by yourself
 
@@ -68,7 +70,7 @@ for i in range (max_epoch_num):
     loss, pred = my_model(data_bt, label_bt)
 
     # backward loss
-    my_model.backward(.....)
+    my_model.backward(loss)
 
     # update parameters in model
     my_model.update_param()
